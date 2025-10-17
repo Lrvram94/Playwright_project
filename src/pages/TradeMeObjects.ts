1b6d6a10efaf5efe41953859a5f97ac9e6275ea4
Lrvram94 <lrvram94@gmail.com>
2025-10-17 15:54:17 +1300

    Reorganize project structure and update test files

--- src/pages/TradeMeObjects.ts
+++ src/pages/TradeMeObjects.ts
@@ -6,69 +6,62 @@ export class TradeMeObjects extends BasePage {
       super(page);
 }
 
-    // TradeMe URLs    
-    get tradeMeSandBoxUrl(): string { return 'https://www.tmsandbox.co.nz/a/'; }
-    get motorsUrl(): string { return 'https://www.tmsandbox.co.nz/a/motors'; }
-    get usedCarsUrl(): string { return 'https://www.tmsandbox.co.nz/a/motors/cars/search?vehicle_condition=used'; }
-    get rentalPropertiesUrl(): string { return 'https://www.tmsandbox.co.nz/a/property/residential/rent'; }
-
-
-    // TradeMe Motor Page Locators
-    get motorVehiclesLink(): Locator { 
-        return this.page.locator('.tm-homepage-search-header__vertical-links-link', { hasText: 'Motors'}); 
-    }
+// TradeMe Motor Page Locators
+get motorVehiclesLink(): Locator { 
+    return this.page.locator('.tm-homepage-search-header__vertical-links-link', { hasText: 'Motors'}); 
+}
     
-    get CarTypeDropdown(): Locator { 
-        return this.page.locator('span.tm-motors-search-bar__dropdown-multi-select-text', { hasText: 'All cars' });
-    }
+get CarTypeDropdown(): Locator { 
+    return this.page.locator('span.tm-motors-search-bar__dropdown-multi-select-text', { hasText: 'All cars' });
+}
      
-    get viewListingsButton(): Locator { 
-        return this.page.locator('button:has-text("View listings")');
-    }
+get viewListingsButton(): Locator { 
+    return this.page.locator('button:has-text("View listings")');
+}
 
-    get searchResultsList(): Locator {
-        return this.page.locator('.tm-search-results');
-    }
+get searchResultsList(): Locator {
+    return this.page.locator('.tm-search-results');
+}
 
-    get propertiesLink(): Locator { 
-        return this.page.locator('.tm-homepage-search-header__vertical-links-link', { hasText: 'Property'}); 
-    }
+get propertiesLink(): Locator { 
+    return this.page.locator('.tm-homepage-search-header__vertical-links-link', { hasText: 'Property'}); 
+}
 
-    get rentalPropertiesLink(): Locator { 
-        return this.page.locator('a.tm-property-search-form__tab-item', { hasText: ' For rent '});    
-   }
+get rentalPropertiesLink(): Locator { 
+    return this.page.locator('a.tm-property-search-form__tab-item', { hasText: ' For rent '});    
+}
 
-   get regionDropdown(): Locator {
-        return this.page.locator('select[name="region"]');
-   }  
+get regionDropdown(): Locator {
+    return this.page.locator('select[name="region"]');
+}  
    
-   get districtDropdown(): Locator {
-        return this.page.locator('select[name="district"]');   
-   }
+get districtDropdown(): Locator {
+    return this.page.locator('select[name="district"]');   
+}
 
-   get allSuburbsDropdown(): Locator {
-        return this.page.locator('button.tm-property-location-search-multi-dropdown__suburb-button', { hasText: "All suburbs" });
-   } 
+get allSuburbsDropdown(): Locator {
+    return this.page.locator('button.tm-property-location-search-multi-dropdown__suburb-button', { hasText: "All suburbs" });
+} 
 
-   get minRentDropdown(): Locator {
-        return this.page.locator('select[name="selectedMin"]');
-   }
+get minRentDropdown(): Locator {
+    return this.page.locator('select[name="selectedMin"]');
+}
 
-   get bedroomsDropdown(): Locator {
-        return this.page.locator('select[name="selectBedrooms"]');
-   }
+get bedroomsDropdown(): Locator {
+    return this.page.locator('select[name="selectBedrooms"]');
+}
 
-   get bathroomsDropdown(): Locator {
-        return this.page.locator('select[name="selectBathrooms"]');
-   }
+get bathroomsDropdown(): Locator {
+    return this.page.locator('select[name="selectBathrooms"]');
+}
 
-   get propertyTypeDropdown(): Locator {
-        return this.page.locator('select[id="propertyTypeSearchButton"]');
-   }
-    get searchResultsButton(): Locator {
-        return this.page.locator('button', { hasText: "Search" });
-    }
-    get rentalSearchResultsList(): Locator {
-        return this.page.locator('tm-property-search-results');
-    }
+get propertyTypeDropdown(): Locator {
+    return this.page.locator('select[id="propertyTypeSearchButton"]');
+}
+get searchResultsButton(): Locator {
+    return this.page.locator('button', { hasText: "Search" });
+}
+get rentalSearchResultsList(): Locator {
+    return this.page.locator('tm-property-search-results');
+}
 }
--- tests/api-tests/jsonplaceholder.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { JSONPlaceholderAPI } from '../../src/api/JSONPlaceholderAPI';
-
-test.describe('JSONPlaceholder API Tests', () => {
-  let api: JSONPlaceholderAPI;
-
-
-});
